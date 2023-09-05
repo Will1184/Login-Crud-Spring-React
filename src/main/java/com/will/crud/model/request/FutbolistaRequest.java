aaf738911570ad59cce113d6ff63271a9edfc5b8
Will1184 <brandonwilliammg@gmail.com>
2023-09-05 16:18:09 -0600

    feat: se agrego la clase para evitar usar la entidad Futbolista directamente en las consultas Http

---
+++ src/main/java/com/will/crud/model/request/FutbolistaRequest.java
@@ -0,0 +1,29 @@
+package com.will.crud.model.request;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FutbolistaRequest {
+    @NotEmpty
+    private String nombres; // Nombres de la persona
+    @NotEmpty
+    private String apellidos; // Apellidos de la persona
+
+    private LocalDate fechaNacimiento; // Fecha de nacimiento de la persona
+    @NotEmpty
+    private String email; // Correo electrónico de la persona
+    @NotEmpty
+    private String telefono; // Número de teléfono de la persona
+    @NotEmpty
+    private String posicion; // Posición en la que juega el futbolista
+}
