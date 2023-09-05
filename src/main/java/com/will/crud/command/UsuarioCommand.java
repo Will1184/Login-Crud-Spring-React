4ba52b82709e81377b6d17085fa0eb0e97f6b382
Will1184 <brandonwilliammg@gmail.com>
2023-09-04 21:57:28 -0600

    feat: se agrego una clase para la creacion automatica de registros de usuarios

---
+++ src/main/java/com/will/crud/command/UsuarioCommand.java
@@ -0,0 +1,47 @@
+package com.will.crud.command;
+
+import com.will.crud.model.enums.Rol;
+import com.will.crud.model.request.RegisterRequest;
+import com.will.crud.service.AuthenticationService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UsuarioCommand implements CommandLineRunner {
+    private final AuthenticationService service;
+
+    public UsuarioCommand(AuthenticationService service) {
+        this.service = service;
+    }
+
+    @Override
+    public void run(String... args) {
+
+        service.register(RegisterRequest.builder()
+                .firstname("Brandon")
+                .lastname("Gomez")
+                .email("example@example.com")
+                .username("Araragi")
+                .password("12345")
+                .rol(Rol.ADMIN)
+                .build());
+
+        service.register(RegisterRequest.builder()
+                .firstname("Brandon")
+                .lastname("Gomez")
+                .email("example1@example.com")
+                .username("Lulu")
+                .password("12345")
+                .rol(Rol.MANAGER)
+                .build());
+
+        service.register(RegisterRequest.builder()
+                .firstname("Brandon")
+                .lastname("Gomez")
+                .email("example2@example.com")
+                .username("ML")
+                .password("12345")
+                .rol(Rol.ADMIN)
+                .build());
+    }
+}
